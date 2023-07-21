dd649ef9b41eb9fb9bc91ae7e27cee23b4b3e40a
RahulDB2k18 <rahuldb2k18@gmail.com>
2023-07-21 14:59:26 +0530

    added global exception by ControllerAdvice and ExceptionHandler

---
+++ src/main/java/com/eazybytes/eazyschool/controller/GlobalExceptionController.java
@@ -0,0 +1,16 @@
+package com.eazybytes.eazyschool.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class GlobalExceptionController {
+    @ExceptionHandler(Exception.class)
+    public ModelAndView excceptionHandler(Exception exception){
+        ModelAndView errorPage = new ModelAndView();
+        errorPage.setViewName("error");
+        errorPage.addObject("errormsg",exception.getMessage());
+        return errorPage;
+    }
+}
